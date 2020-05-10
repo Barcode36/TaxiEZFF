bb3557e6788c966fcff88951121ca8e7cbceb664
VicEspino <taxgype1@gmail.com>
2020-05-10 04:13:25 -0500

    ServiciosProgramadosCRUDontroller Interacción de radio button y combobox de dias hecha, con su validación en el metodo de validaciones.
    Al checkear los dias se elejirá el dia más cercano en el date picker con base a los dias checkeado y al dia actual.

--- src/controllers/crudsControllers/ServiciosProgramadosCrudController.java
+++ src/controllers/crudsControllers/ServiciosProgramadosCrudController.java
@@ -5,18 +5,37 @@ import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTimePicker;
+import com.jfoenix.controls.base.IFXValidatableControl;
+import com.jfoenix.validation.RequiredFieldValidator;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import models.interfaces.IValidateCRUD;
 import models.interfaces.Registro;
 import models.interfaces.SetAddRegistroListener;
+import services.StringLengthValidator;
+import services.sql.ClienteSQL;
 
-public class ServiciosProgramadosCrudController extends SetAddRegistroListener {
+import java.net.URL;
+import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ServiciosProgramadosCrudController extends SetAddRegistroListener implements Initializable, IValidateCRUD {
 
     @FXML
     private AnchorPane root;
@@ -47,9 +66,6 @@ public class ServiciosProgramadosCrudController extends SetAddRegistroListener {
 
     @FXML
     private JFXTextField textField_notas;
-
-    @FXML
-    private JFXTextField textField_observaciones;
 
     @FXML
     private JFXRadioButton rb_personalizado;
@@ -99,6 +115,74 @@ public class ServiciosProgramadosCrudController extends SetAddRegistroListener {
     @FXML
     private Label lbl_errorConfigTipoServicio;
 
+    LocalDate ldLunesProximo;
+    LocalDate ldMartesProximo;
+    LocalDate ldMiercolesProximo;
+    LocalDate ldJuevesProximo;
+    LocalDate ldViernesProximo;
+    LocalDate ldSabadoProximo;
+    LocalDate ldDomingoProximo;
+    LocalDate ldHoy;
+
+    private ArrayList<JFXCheckBox> listaCheckBox = new ArrayList();
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        setFieldValidations();
+        setClickedEventCheckBox();
+
+
+        this.rb_diario.setOnAction((ActionEvent event) -> {
+            //toogleCheckBoxDias();
+            setChecksDays(true);
+
+            datePicker_dia.setValue(LocalDate.now());
+            timePicker_horaServicio.setValue(LocalTime.now());
+        });
+
+        this.rb_personalizado.setOnAction(event -> {
+
+            datePicker_dia.setValue(getDiaMasCercanoInicio());
+
+        });
+
+    }
+
+    private LocalDate getDiaMasCercanoInicio() {
+
+        ldHoy = LocalDate.now();
+
+        //alguno de estos tiene el mismo dia que hoy.
+        ldLunesProximo = cb_lunes.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)):null;
+        ldMartesProximo = cb_martes.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY)):null;
+        ldMiercolesProximo = cb_miercoles.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.WEDNESDAY)):null;
+        ldJuevesProximo = cb_jueves.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY)):null;
+        ldViernesProximo = cb_viernes.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY)):null;
+        ldSabadoProximo = cb_sabado.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY)):null;
+        ldDomingoProximo = cb_domingo.isSelected() ?ldHoy.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)):null;
+
+        LocalDate diaMasCercano = ldLunesProximo;
+        //ALgunos pueden ser nulos, entonces:
+        if(ldMartesProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldMartesProximo)||diaMasCercano.isEqual(ldMartesProximo)?ldMartesProximo:diaMasCercano;
+        if(ldMiercolesProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldMiercolesProximo)||diaMasCercano.isEqual(ldMiercolesProximo)?ldMiercolesProximo:diaMasCercano;
+        if(ldJuevesProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldJuevesProximo)||diaMasCercano.isEqual(ldJuevesProximo)?ldJuevesProximo:diaMasCercano;
+        if(ldViernesProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldViernesProximo)||diaMasCercano.isEqual(ldViernesProximo)?ldViernesProximo:diaMasCercano;
+        if(ldSabadoProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldSabadoProximo)||diaMasCercano.isEqual(ldSabadoProximo)?ldSabadoProximo:diaMasCercano;
+        if(ldDomingoProximo!=null)
+            diaMasCercano = diaMasCercano.isBefore(ldDomingoProximo)||diaMasCercano.isEqual(ldDomingoProximo)?ldDomingoProximo:diaMasCercano;
+
+
+        return diaMasCercano;
+    }
+
+
     @FXML
     void DetectFocusable_OnMouse(MouseEvent event) {
 
@@ -128,4 +212,168 @@ public class ServiciosProgramadosCrudController extends SetAddRegistroListener {
     public Registro guardarCambiosRegistros() {
         return null;
     }
+
+
+    @Override
+    public ArrayList<IFXValidatableControl> listControlsRequired() {
+        return null;
+    }
+
+    @Override
+    public void setFieldValidations() {
+
+        this.setLengthValidation();
+        this.setRequiredValidation();
+        this.setFocusedProperty();
+
+        textField_telefono_buscar.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+                extraerRegistro(new ClienteSQL().existe(newValue));
+            } catch (SQLException e) {
+                e.printStackTrace();
+                //ventana error.
+            }
+        });
+    }
+
+    @Override
+    public void setLengthValidation() {
+
+        textField_telefono.getValidators().add(new StringLengthValidator("Máximo 11 carácteres.",11));
+        textField_colonia.getValidators().add(new StringLengthValidator("Máximo 45 carácteres.",45));
+        textField_calle.getValidators().add(new StringLengthValidator("Máximo 45 carácteres.",45));
+        textField_num_ext.getValidators().add(new StringLengthValidator("Máximo 10 carácteres.",10));
+        textField_numInt.getValidators().add(new StringLengthValidator("Máximo 10 carácteres.",10));
+
+
+    }
+
+    @Override
+    public void setRequiredValidation() {
+        textField_telefono.getValidators().add(new RequiredFieldValidator("Este campo es requerido."));
+        textField_colonia.getValidators().add(new RequiredFieldValidator("Este campo es requerido."));
+        textField_calle.getValidators().add(new RequiredFieldValidator("Este campo es requerido."));
+        datePicker_dia.getValidators().add(new RequiredFieldValidator("Este campo es requerido."));
+        timePicker_horaServicio.getValidators().add(new RequiredFieldValidator("Este campo es requerido."));
+    }
+
+    private void setFocusedProperty() {
+
+        textField_nombre.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                textField_telefono.validate();
+        });
+
+        textField_colonia.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                textField_colonia.validate();
+        });
+        textField_calle.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                textField_calle.validate();
+        });
+        datePicker_dia.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                datePicker_dia.validate();
+        });
+        timePicker_horaServicio.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                timePicker_horaServicio.validate();
+        });
+        textField_numInt.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                textField_numInt.validate();
+        });
+        textField_num_ext.focusedProperty().addListener((observable,  oldValue,  newValue)-> {
+            if(!newValue)
+                textField_num_ext.validate();
+        });
+
+
+
+    }
+
+    @Override
+    public boolean validarCampos() {
+        ObservableList<Node> listaHijos = root.getChildren();
+        boolean validacioExitosa = true;
+
+
+        for(Node node : listaHijos){
+            if(node instanceof IFXValidatableControl){
+                boolean validate = ((IFXValidatableControl) node).validate();
+                validacioExitosa = validacioExitosa&&validate;
+            }
+        }
+
+        if(!isAnyDayChek()) {
+            validacioExitosa = false;
+            //ventana o mensaje error diciendo que necesita seleccionar al menos un dia
+        }
+        //validacioExitosa &= isAnyDayChek();
+
+        return validacioExitosa;
+    }
+
+
+    private void setChecksDays(boolean state){
+        this.cb_lunes.selectedProperty().set(state);
+        this.cb_martes.selectedProperty().set(state);
+        this.cb_miercoles.selectedProperty().set(state);
+        this.cb_jueves.selectedProperty().set(state);
+        this.cb_viernes.selectedProperty().set(state);
+        this.cb_sabado.selectedProperty().set(state);
+        this.cb_domingo.selectedProperty().set(state);
+    }
+
+    /**
+     * Cuando se manipula un checkbox de los dias entonces significa que el servicio es
+     * programado personalizado, al menos que los 7 checks estén activos ahí se queda como servicio
+     * programado diario.
+     */
+    private void setClickedEventCheckBox(){
+
+
+        listaCheckBox.add(this.cb_lunes);
+        listaCheckBox.add(this.cb_martes);
+        listaCheckBox.add(this.cb_miercoles);
+        listaCheckBox.add(this.cb_jueves);
+        listaCheckBox.add(this.cb_viernes);
+        listaCheckBox.add(this.cb_sabado);
+        listaCheckBox.add(this.cb_domingo);
+
+        for(JFXCheckBox actualCB : listaCheckBox){
+            actualCB.setOnAction(new EventHandler<ActionEvent>() {
+                 @Override
+                 public void handle(ActionEvent event) {
+
+                     rb_personalizado.selectedProperty().set(true);
+
+                     if(isAllDaysChecked()){
+                         rb_diario.fire();
+                     }
+                 }
+             });
+        }
+    }
+
+    private boolean isAllDaysChecked(){
+        boolean check = true;
+
+        for(JFXCheckBox cb_actual : listaCheckBox){
+            check &= cb_actual.isSelected();
+        }
+        return check;
+    }
+
+    private boolean isAnyDayChek(){
+        boolean check = false;
+        for(JFXCheckBox cb_actual : listaCheckBox){
+            check |= cb_actual.isSelected();
+        }
+        return check;
+    }
+
+
 }
+
